bf01495b89f7b8c71fbf277df7c8d0c2920e3bb5
Taddei Laetitia <laetdi@gmail.com>
2022-07-31 19:11:09 +0200

    check if element is in the viewport

--- style/vendors/main.js
+++ style/vendors/main.js
@@ -1,8 +1,16 @@
 
 const section = document.querySelectorAll('.section')
 
-section.forEach((section) => {
+const inViewport = (e) => {
     const rect = section.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+)}
+
+section.forEach((section) => {
     return (
         console.log(rect)
     )
