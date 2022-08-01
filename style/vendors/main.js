9ddf2d5fafccbea08109567f568b6fca360acc3b
Taddei Laetitia <laetdi@gmail.com>
2022-08-01 11:38:13 +0200

    js getting the active class of navDots

--- style/vendors/main.js
+++ style/vendors/main.js
@@ -1,21 +1,20 @@
-const isInViewport = (e) => {
-    const rect = e.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+easyScrollDots({
+    'fixedNav': true,
+    'fixedNavId': 'nvbar',
+    'fixedNavUpward': false,
+    'offset': 56
+    });
 
-const section = document.getElementById('about')
-const links = document.querySelector('.links-item')
 
-document.addEventListener('scroll', function() {
-    if (isInViewport(section) === true) {
-        links.classList.add('active')
-    } {
-        passive: true
+const navDots = document.querySelectorAll('.scroll-indicator-controller > div')
+const links = document.querySelectorAll('.links-item')
+
+navDots.forEach(dot => {
+        if(dot.classList.contains('active') == true){
+
+        return console.log(dot.attributes[1].name)
     }
-})
+}
+)
+
 
