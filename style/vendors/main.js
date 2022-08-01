c04fdf5f57ef6c6d142f4806fdde69a25bf94fcc
Taddei Laetitia <laetdi@gmail.com>
2022-08-01 15:05:37 +0200

    matching the values

--- style/vendors/main.js
+++ style/vendors/main.js
@@ -12,22 +12,22 @@ const links = document.querySelectorAll('.links-item')
 /* const about = links[0].innerText.toLowerCase()
 const projects = links[1].innerText.toLowerCase()
 const contact = links[2].innerText.toLowerCase() */
-/* console.log(about)
+/* console.log(about)*/
+
+
 const activeLink = (value) => {
     links.forEach(link => {
-        if(link.innerText.toLowercase() == value){
+        if(link.innerText.toLowerCase() == value){
             console.log(link)
         }
     })
-} */
+}
 
 document.addEventListener('scroll', () => {
     navDots.forEach(dot => {
         if(dot.classList.contains('active') == true){
         let nodeValue = dot.attributes[1].nodeValue
-        }
-        links.forEach(link => {
-            console.log (link.innerText.toLowerCase() == nodeValue)
-        })
-    })
+        activeLink(nodeValue)
+    }
+})
 })
